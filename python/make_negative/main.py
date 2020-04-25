13940ebd23dfb660caae64fdd77daa8804ed4194
Tardigrada777 <ntwrite@gmail.com>
2020-04-25 21:19:33 +0300

    fix(python): fix function names for make negative solution

--- python/make_negative/main.py
+++ python/make_negative/main.py
@@ -1,6 +1,7 @@
 def make_negative( number ):
     return number if number < 0 else -number
 
+
 # better solution
-def make_negative( number ):
+def make_negative2( number ):
     return -abs(number)
